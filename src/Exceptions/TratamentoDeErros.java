002bf8e6fd6234f92dad73faa1377c9c26d8e062
brjoaof <brjoaof@gmail.com>
2020-08-12 22:19:14 -0300

    Update Exception

--- src/Exceptions/TratamentoDeErros.java
+++ src/Exceptions/TratamentoDeErros.java
@@ -1,5 +1,23 @@
 package Exceptions;
 
-public class TratamentoDeErros {
+public class TratamentoDeErros extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public TratamentoDeErros() {
+	        super();
+	    }
+
+	    public TratamentoDeErros(String message) {
+	        super(message);
+	    }
+
+	    public TratamentoDeErros(String message, Throwable cause) {
+	        super(message, cause);
+	    }
+
+	    public TratamentoDeErros(Throwable cause) {
+	        super(cause);
+	    }
 
 }
